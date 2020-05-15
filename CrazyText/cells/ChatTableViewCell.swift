4965dbad1f12ad624c1ff7979a45c1df8e464c37
David <l.rodrigues.david@gmail.com>
2020-05-15 12:33:43 +0200

    add text detection

--- CrazyText/cells/ChatTableViewCell.swift
+++ CrazyText/cells/ChatTableViewCell.swift
@@ -52,8 +52,73 @@ class ChatTableViewCell: UITableViewCell {
     }
     
     func set(message: String, incoming: Bool) {
-        self.message.text = message
+        let attributedString = NSMutableAttributedString.init(string: message)
+        phoneNumber(in: message).forEach {
+            attributedString.addAttribute(NSAttributedString.Key.underlineStyle, value: 1, range: $0)
+        }
+
+        email(in: message).forEach {
+            attributedString.addAttribute(NSAttributedString.Key.underlineStyle, value: 1, range: $0)
+        }
+        
+        url(in: message).forEach {
+            attributedString.addAttribute(NSAttributedString.Key.underlineStyle, value: 1, range: $0)
+        }
+        
+        dates(in: message).forEach {
+            attributedString.addAttribute(NSAttributedString.Key.underlineStyle, value: 1, range: $0)
+        }
+        
+        self.message.attributedText = attributedString
         self.isIncoming = incoming
+    }
+    
+    func email(in text: String) -> [NSRange] {
+        var ranges = [NSRange]()
+        let split = text.split(separator: " ")
+        split.forEach {
+            if let range = $0.range(of: "[A-Z0-9a-z.-_]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,3}", options: .regularExpression) {
+                ranges.append(NSRange(text.range(of: text[range])!, in: text))
+            }
+        }
+        
+        return ranges
+    }
+    
+    func url(in text: String) -> [NSRange] {
+        var ranges = [NSRange]()
+        let split = text.split(separator: " ")
+        split.forEach {
+            if let range = $0.range(of: #"/(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/"#, options: .regularExpression) {
+                ranges.append(NSRange(text.range(of: text[range])!, in: text))
+            }
+        }
+        
+        return ranges
+    }
+    
+    func dates(in text: String) -> [NSRange] {
+        var ranges = [NSRange]()
+        let split = text.split(separator: " ")
+        split.forEach {
+            if let range = $0.range(of: "^([0-2][0-9]||3[0-1])/(0[0-9]||1[0-2])/([0-9][0-9])?[0-9][0-9]$", options: .regularExpression) {
+                ranges.append(NSRange(text.range(of: text[range])!, in: text))
+            }
+        }
+        
+        return ranges
+    }
+    
+    func phoneNumber(in text: String) -> [NSRange] {
+        var ranges = [NSRange]()
+        let split = text.split(separator: " ")
+        split.forEach {
+            if let range = $0.range(of: #"^(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})$"#, options: .regularExpression) {
+                ranges.append(NSRange(text.range(of: text[range])!, in: text))
+            }
+        }
+        
+        return ranges
     }
     
     required init?(coder: NSCoder) {
